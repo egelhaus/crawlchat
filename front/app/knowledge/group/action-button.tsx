7d1419f3c515f92bdbe51d1198ad814ff29f5f90
Pramod <pramodkumar.damam73@gmail.com>
2025-08-01 12:28:24 +0530

    Solid button if not fetched yet

--- front/app/knowledge/group/action-button.tsx
+++ front/app/knowledge/group/action-button.tsx
@@ -1,8 +1,9 @@
 import { IconButton } from "@chakra-ui/react";
 import type { KnowledgeGroup } from "libs/prisma";
 import { useEffect } from "react";
-import { TbPlayerStopFilled, TbRefresh } from "react-icons/tb";
+import { TbArrowRight, TbPlayerStopFilled, TbRefresh } from "react-icons/tb";
 import { useFetcher } from "react-router";
+import { Button } from "~/components/ui/button";
 import { toaster } from "~/components/ui/toaster";
 import { Tooltip } from "~/components/ui/tooltip";
 
@@ -47,14 +48,25 @@ export function ActionButton({
         >
           <input type="hidden" name="intent" value="refresh" />
           <Tooltip showArrow content="Update the items of the group">
-            <IconButton
-              size={buttonSize}
-              variant={"subtle"}
-              type="submit"
-              disabled={refreshFetcher.state !== "idle"}
-            >
-              <TbRefresh />
-            </IconButton>
+            {group.status === "pending" ? (
+              <Button
+                colorPalette={"brand"}
+                type="submit"
+                disabled={refreshFetcher.state !== "idle"}
+              >
+                Fetch now
+                <TbArrowRight />
+              </Button>
+            ) : (
+              <IconButton
+                size={buttonSize}
+                variant={"subtle"}
+                type="submit"
+                disabled={refreshFetcher.state !== "idle"}
+              >
+                <TbRefresh />
+              </IconButton>
+            )}
           </Tooltip>
         </refreshFetcher.Form>
       )}
