543d4a910f32face004e8281a65f0020e913362a
Pramod <pramodkumar.damam73@gmail.com>
2025-08-07 20:54:59 +0530

    Track vemetric events

--- front/app/pirsch.tsx
+++ front/app/pirsch.tsx
@@ -1,9 +1,13 @@
+import { vemetric } from "@vemetric/react";
+
 export function track(name: string, meta: Record<string, any>) {
   const prisch = (window as any).pirsch;
   if (!prisch) {
     console.error("Pirsch is not initialized. Ignoring event.", name, meta);
     return;
   }
+
+  vemetric.trackEvent(name, meta);
 
   prisch(name, { meta });
 }
